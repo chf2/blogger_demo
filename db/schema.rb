65fe8c455303d9c0acc3c0b3c47c54263636ed35
Charles Francis <charles.h.francis@gmail.com>
2015-07-14 10:17:45 -0700

    add a user and comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713224620) do
+ActiveRecord::Schema.define(version: 20150714170123) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
     t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
